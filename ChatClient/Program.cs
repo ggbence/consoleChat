87e6e2fc13f6f4db58d16cee58da615a19e55def
ggbence <gorogbence@gmail.com>
2015-02-26 22:29:27 +0100

    elso

---
+++ ChatClient/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Text;
+using System.Net.Sockets ;
+using System.Threading;
+
+namespace ChatClient
+{
+    class Program
+    {
+        private System.Net.Sockets.TcpClient clientSocket;
+        private NetworkStream serverStream;
+        private string readData;
+
+        private string ipAddress;
+        private string userName;
+
+        public Program()
+        {
+            ipAddress = "";
+            userName = "";
+
+            clientSocket = new System.Net.Sockets.TcpClient();
+            serverStream = default(NetworkStream);
+            readData = null;
+        }
+        
+        // send button
+        private void sendMessage(string sendData)
+        {
+            byte[] outStream = System.Text.Encoding.ASCII.GetBytes(sendData + "$");
+            serverStream.Write(outStream, 0, outStream.Length);
+            serverStream.Flush();
+        }
+
+        // connection to the server
+        private void Connection(string ipAddress)
+        {
+            try
+            {
+                this.clientSocket.Connect(ipAddress, 8888);
+
+                this.serverStream = clientSocket.GetStream();
+                readData = "Conected to Chat Server ...";
+                msg(readData);
+
+                byte[] outStream = System.Text.Encoding.ASCII.GetBytes(this.userName + "$");
+                serverStream.Write(outStream, 0, outStream.Length);
+                serverStream.Flush();
+
+                Thread ctThread = new Thread(getMessage);
+                ctThread.Start();
+
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine("Could not connect to the server.");
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+           
+            
+        }
+
+        // get messages
+        private void getMessage() 
+        {
+            while (true)
+            {
+                try
+                {
+                    serverStream = clientSocket.GetStream();
+                    int buffSize = 0;
+                    byte[] inStream = new byte[10025];
+                    buffSize = clientSocket.ReceiveBufferSize;
+                    serverStream.Read(inStream, 0, buffSize);
+                    string returndata = System.Text.Encoding.ASCII.GetString(inStream);
+                    readData = "" + returndata;
+                    msg(readData.Replace("\0", ""));
+                }
+                catch (Exception ex)
+                {
+                    
+                   Thread.CurrentThread.Abort();
+                    throw;
+                }
+                
+            }
+        }
+
+        // write the message
+        private void msg(string data)
+        {
+            /*
+            if (this.InvokeRequired)
+                this.Invoke(new MethodInvoker(msg));
+            else
+                textBox1.Text = textBox1.Text + Environment.NewLine + " >> " + readData;
+             */
+            if ((!data.Equals("")) &&(data != null))
+            {
+                Console.WriteLine(data);
+            }
+            
+        }
+
+        private void run()
+        {
+            try
+            {
+                Connection(this.ipAddress);
+                while (true)
+                {
+                    string message = Console.ReadLine();
+                    sendMessage(message);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Communication error. Program will be terminated.");
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+                return;
+            }
+            
+
+        }
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            Console.Write("server: ");
+            program.ipAddress = Console.ReadLine();
+            Console.Write("user: ");
+            program.userName = Console.ReadLine();
+
+            program.run();
+            
+
+        }
+    }
+}
---
+++ windowsChatClient/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace windowsChatClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.lblServer = new System.Windows.Forms.Label();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.txtBox = new System.Windows.Forms.RichTextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(184, 13);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(160, 20);
+            this.txtServer.TabIndex = 0;
+            this.txtServer.TextChanged += new System.EventHandler(this.txtServer_TextChanged);
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(184, 40);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(159, 20);
+            this.txtUserName.TabIndex = 1;
+            this.txtUserName.TextChanged += new System.EventHandler(this.txtUserName_TextChanged);
+            // 
+            // lblServer
+            // 
+            this.lblServer.AutoSize = true;
+            this.lblServer.Location = new System.Drawing.Point(143, 16);
+            this.lblServer.Name = "lblServer";
+            this.lblServer.Size = new System.Drawing.Size(41, 13);
+            this.lblServer.TabIndex = 2;
+            this.lblServer.Text = "Server:";
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Location = new System.Drawing.Point(126, 43);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(58, 13);
+            this.lblUserName.TabIndex = 3;
+            this.lblUserName.Text = "Username:";
+            // 
+            // txtBox
+            // 
+            this.txtBox.Location = new System.Drawing.Point(14, 109);
+            this.txtBox.Name = "txtBox";
+            this.txtBox.Size = new System.Drawing.Size(331, 248);
+            this.txtBox.TabIndex = 4;
+            this.txtBox.Text = "";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(14, 383);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(330, 20);
+            this.txtMessage.TabIndex = 5;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(15, 409);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(330, 36);
+            this.btnSend.TabIndex = 6;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(184, 67);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(158, 23);
+            this.btnConnect.TabIndex = 7;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(356, 457);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtBox);
+            this.Controls.Add(this.lblUserName);
+            this.Controls.Add(this.lblServer);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.txtServer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label lblServer;
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.RichTextBox txtBox;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnConnect;
+    }
+}
+
